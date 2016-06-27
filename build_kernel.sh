715cec7fb783eda91a288cde9d43df53a558ea06
WildfireDEV <wildfiredev@gmail.com>
2016-06-27 11:54:27 +1000

    Stop cleaning everytime

--- build_kernel.sh
+++ build_kernel.sh
@@ -126,29 +126,6 @@ replace_file() {
   chmod $2 $1;
 }
 
-CLEAN()
-{
-	echo ""
-	echo "=============================================="
-	echo "START: MAKE CLEAN"
-	echo "=============================================="
-	echo ""
-	make clean
-	make distclean
-	make mrproper
-	find . -name "*.dtb" -exec rm {} \;
-	find . -type f -name "*~" -exec rm -f {} \;
-	find . -type f -name "*orig" -exec rm -f {} \;
-	find . -type f -name "*rej" -exec rm -f {} \;
-	find . -name "*.ko" -exec rm {} \;
-	ccache -C	
-	echo ""
-	echo "=============================================="
-	echo "END: MAKE CLEAN"
-	echo "=============================================="
-	echo ""
-}
-
 CLEANCONFIG()
 {	
 	echo ""
@@ -380,7 +357,6 @@ SEANDROIDENFORCE()
 
 BUILD_KERNEL_G920F()
 {	
-	CLEAN
 	echo ""
 	echo "=============================================="
 	echo "START: BUILD_KERNEL"
@@ -418,7 +394,6 @@ BUILD_KERNEL_G925F()
 
 BUILD_KERNEL_G920FWSM()
 {	
-	CLEAN
 	echo ""
 	echo "=============================================="
 	echo "START: BUILD_KERNEL"
